0233b58bf5cca6ffc9efb5865d947ecefa048803
geomensor90 <paivaagrimensor@gmail.com>
2025-04-04 13:27:44 -0300

    04-04-2025

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ dados = {
     "Ano": [2020, 2021, 2022, 2023, 2024, 2025],
     "Coluna 2": [1.71, 1.8, 2, 2.12, 2.2, 2.31],
     "Coluna 3": [0.23, 0.24, 0.27, 0.29, 0.3, 0.31],
-    "Coluna 4": [0.15, 0.16, 0.17, 0.18, 0.19, 0.2]
+    "Coluna 4": [0.15, 0.16, 0.17, 0.18, 0.19, 0.2],
+    "Coluna 5": [36.30, 38.16, 42.34, 44.87, 46.60, 48.86],
 }
 
 df = pd.DataFrame(dados)
@@ -37,11 +38,6 @@ def pagina_teo():
         return
     
     # Cálculo dos valores
-    resultados = []
-    soma_total = 0
-    exct = 0
-    area_real = 0 
-    
     for _, row in dados_filtrados.iterrows():
         if campo1 <= 1000:
             resultado = campo1 * row["Coluna 2"]
@@ -52,22 +48,23 @@ def pagina_teo():
             area_real = 1000
             exct = campo1 - 1000
 
-        soma_total += resultado
-
         # Exibição dos resultados
         st.subheader(f"**Ano {int(row['Ano'])}**")
-        st.write(f"Fica o responsável pela execução da obra, AUTUADO por não efetuar a declaração da Taxa de Execução de Obras - TEO, referente ao exercício {int(row['Ano'])}")
+        st.write(f"Fica o contribuinte AUTUADO por não efetuar a declaração da Taxa de Execução de Obras - TEO, referente ao exercício {int(row['Ano'])}")
         st.write(f"Memória de Cálculo:")        
         st.write(f"Metro quadrado até áreas de 1000m²= R$ {row['Coluna 2']:.2f}")
         st.write(f"Valor excedente = R$ {row['Coluna 3']:.2f}")
         st.write(f"Área (m²): {campo1:.2f}")
         st.write(f"Valor até 1000m² = {area_real:.2f} X {row['Coluna 2']:.2f} = R$ {(area_real * row['Coluna 2']):.2f}")
         st.write(f"Valor excedente = {exct:.2f} X {row['Coluna 3']:.2f} = R$ {(exct * row['Coluna 3']):.2f}")
-        st.write(f"Valor total para {int(row['Ano'])}: R$ {resultado:.2f}")     
+        st.write(f"Valor total para {int(row['Ano'])}: R$ {resultado:.2f}")
+        # Verifica se o resultado é menor que o valor mínimo
+        if resultado < row["Coluna 5"]:
+            st.write(f"**Atenção! O valor mínimo da TEO para o ano {int(row['Ano'])} é de R$ {row['Coluna 5']:.2f}**")     
         st.write(f"Prazo: 30 dias")
-        st.write(f"§ 1º Na hipótese de recolhimento integral da taxa, o valor da multa prevista no caput será reduzido em 80% (oitenta por cento) se o pagamento for efetuado no prazo de até 20 (vinte) dias contados da data em que o contribuinte ou responsável for notificado da exigência.")          
+        st.write(f"LC 783/2008, art. 20, § 1º: Na hipótese de recolhimento integral da taxa, o valor da multa prevista no caput será reduzido em 80% (oitenta por cento) se o pagamento for efetuado no prazo de até 20 (vinte) dias contados da data em que o contribuinte ou responsável for notificado da exigência.")          
         st.write(f"--------------------------------------")
-
 
 if __name__ == "__main__":
     pagina_teo()
+
